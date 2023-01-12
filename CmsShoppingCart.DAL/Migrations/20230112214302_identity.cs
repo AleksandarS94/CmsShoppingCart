541255e790f2aec0d3afddae23ca2a677f96d2dd
Aleksandar94 <aleksandar.stevanovic100@gmail.com>
2023-01-12 23:11:15 +0100

    Add New Project

---
+++ CmsShoppingCart.DAL/Migrations/20230112214302_identity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CmsShoppingCart.DAL.Migrations
+{
+    public partial class identity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+           
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+          
+        }
+    }
+}
